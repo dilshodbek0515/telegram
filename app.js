f81fa3c3d15c7c591ea9d3bff05ba9fa678b3ac6
dilshod <dilshodx516@gmail.com>
2024-10-05 11:50:53 +0500

    telegram

---
+++ app.js
@@ -0,0 +1,15 @@
+// const clockEl = document.querySelector(".clock_item");
+
+// {
+//     function currentTime() {
+//         let date = new Date()
+//         let hour = date.getHours().toString().padStart(2, "0")
+//         let minute = date.getMinutes().toString().padStart(2, "0")
+//         clockEl.innerHTML = `${hour} : ${minute}`;
+//     }
+//     currentTime()
+
+//     setInterval(() => {
+//         currentTime()
+//     }, 1000);
+// }
